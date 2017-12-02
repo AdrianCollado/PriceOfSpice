be8307f8580ced5e0b6603321ddab962d9f0219c
Scott Thiel <thiels2@rpi.edu>
2017-12-02 16:50:15 -0500

    ayyyyy its done

--- src/main/java/com/polaritech/minecraft/pos/proxy/ClientProxy.java
+++ src/main/java/com/polaritech/minecraft/pos/proxy/ClientProxy.java
@@ -1,6 +1,9 @@
 package com.polaritech.minecraft.pos.proxy;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,6 +17,13 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
+    }
+
+    @SubscribeEvent
+    public static void registerSounds(RegistryEvent.Register<SoundEvent> event) {
+        ResourceLocation location = new ResourceLocation("pos", "christmas_music");
+        SoundEvent sound = new SoundEvent(location).setRegistryName(location);
+        event.getRegistry().register(sound);
     }
 
     @SubscribeEvent
