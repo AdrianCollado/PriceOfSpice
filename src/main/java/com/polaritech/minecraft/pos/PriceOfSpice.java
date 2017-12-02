236cb65b9d4a132f119638d56dd2609216438bcf
Scott Thiel <thiels2@rpi.edu>
2017-12-02 14:21:52 -0500

    renamed the main file to something proper

--- src/main/java/com/polaritech/minecraft/pos/PriceOfSpice.java
+++ src/main/java/com/polaritech/minecraft/pos/PriceOfSpice.java
@@ -4,13 +4,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = POS.ID, version = POS.VERSION)
-public class POS {
+@Mod(modid = PriceOfSpice.ID, version = PriceOfSpice.VERSION)
+public class PriceOfSpice {
     public static final String ID = "pos";
     public static final String Version = "0.0.1-0";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("POS - Price of Spice");
+        System.out.println("PriceOfSpice - Price of Spice");
     }
 }
